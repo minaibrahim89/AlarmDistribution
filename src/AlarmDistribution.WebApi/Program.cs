845a0c2f84f2a81ac8a5b7a479c161d7634385cc
Mina Ibrahim <iar1fe@bosch.com>
2026-03-17 01:27:42 +0100

    feat(db): run init SQL script on startup if present
    Add support for executing an initializedb.sql script after database creation. The script is included in the project and copied to the output directory, allowing custom initialization logic to be applied automatically at startup.

--- src/AlarmDistribution.WebApi/Program.cs
+++ src/AlarmDistribution.WebApi/Program.cs
@@ -1,5 +1,6 @@
 using AlarmDistribution.WebApi.Extensions;
 using AlarmDistribution.WebApi.Infrastructure.Database.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +17,14 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await dbContext.Database.EnsureCreatedAsync();    
+    await dbContext.Database.EnsureCreatedAsync();
+
+    var initScriptPath = Path.Combine(AppContext.BaseDirectory, "initializedb.sql");
+    if (File.Exists(initScriptPath))
+    {
+        var initScript = await File.ReadAllTextAsync(initScriptPath);
+        await dbContext.Database.ExecuteSqlRawAsync(initScript);
+    }
 }
 
 // Configure the HTTP request pipeline.
